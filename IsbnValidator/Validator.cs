b2c279f254fda026239f3189f240fbd5e8ca2a03
CoolDev <emzari.lukava@gmail.com>
2023-12-10 12:02:47 +0400

    commit1

--- IsbnValidator/Validator.cs
+++ IsbnValidator/Validator.cs
@@ -10,8 +10,21 @@
         /// <exception cref="ArgumentException"><paramref name="isbn"/> is empty or has only white-space characters.</exception>
         public static bool IsIsbnValid(string isbn)
         {
-            // TODO Analyze the method unit tests and implement the method.
-            throw new NotImplementedException();
+            string cleanedIsbn = isbn.Replace("-", string.Empty).Replace(" ", string.Empty);
+
+            if (cleanedIsbn.Length != 10)
+            {
+                return false;
+            }
+
+            int checksum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                int digit = (i == 9 && cleanedIsbn[i] == 'X') ? 10 : int.Parse(cleanedIsbn[i].ToString());
+                checksum += (10 - i) * digit;
+            }
+
+            return checksum % 11 == 0;
         }
     }
 }
